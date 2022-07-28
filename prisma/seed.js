0935fa66956b9a7d067b4c4730da8db9bce11900
GaspardMzt <52825122+GaspardMzt@users.noreply.github.com>
2022-07-28 14:02:17 +0200

    Initialize services

---
+++ prisma/seed.js
@@ -0,0 +1,22 @@
+const { hashPassword } = require("../models/user");
+const db = require("../lib/prisma");
+
+async function seed() {
+  const hashedPassword = await hashPassword("superpassword");
+  await db.user.deleteMany();
+  await db.user.create({
+    data: {
+      email: "dave.lopper@gmail.com",
+      firstname: "Dave",
+      lastname: "Lopper",
+      hashedPassword: hashedPassword,
+      phoneNumber: "0769422558",
+      address: "7 turtle road",
+      society: "Credit Ecolo",
+    },
+  });
+}
+
+seed();
+
+module.exports = seed;

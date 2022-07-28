44cd749924b2b934d1a96bda26a00f295bfd0d87
GaspardMzt <52825122+GaspardMzt@users.noreply.github.com>
2022-07-28 14:02:18 +0200

    Initialize header

---
+++ components/ResaChauffeur.js
@@ -0,0 +1,39 @@
+import ButtNavResa from "./ButtonNavResa";
+import HeaderResa from "../styles/ResaChauffeur.module.css";
+import { useRouter } from "next/router";
+
+export default function ResaChauf() {
+  const router = useRouter();
+
+  const linkToServiceAvcChauff = () => {
+    router.push("/location_avec_chauffeur");
+  };
+
+  const linkToLocaAvecChauff = () => {
+    router.push("/course_dans_lyon");
+  };
+  return (
+    <div className={HeaderResa.resaContainer}>
+      <div className={HeaderResa.title}>
+        <p className={HeaderResa.resaContainerp}>RÉSERVER UN CHAUFFEUR</p>
+      </div>
+      <div className={HeaderResa.buttonsResaChauff}>
+        <ButtNavResa
+          classe={HeaderResa.url1}
+          phrase1="Transfert à l'aéroport Lyon Saint Exupéry"
+        />
+        <ButtNavResa
+          classe={HeaderResa.url2}
+          phrase1="Courses dans Lyon et ses alentours"
+          linkTo={linkToLocaAvecChauff}
+        />
+        <ButtNavResa
+          classe={HeaderResa.url3}
+          phrase1="Location de véhicule avec chauffeur"
+          phrase2="NOUVEAU"
+          linkTo={linkToServiceAvcChauff}
+        />
+      </div>
+    </div>
+  );
+}
